f65b46b106250e26387f03cde29c914ae2025098
Obvodny <obvodny@yeah.net>
2026-03-07 22:11:57 +0800

    add echo

--- utilities/xmake.lua
+++ utilities/xmake.lua
@@ -1,3 +1,30 @@
+target("echo")
+    set_kind("binary")
+    add_files("echo/*.cpp")
+    add_tests("test_echo_0", {
+        trim_output = false,
+        plain = true,
+        pass_outputs = "\n",
+    })
+    add_tests("test_echo_1", {
+        runargs = "hello",
+        trim_output = false,
+        plain = true,
+        pass_outputs = "hello\n",
+    })
+    add_tests("test_echo_2", {
+        runargs = {"你好", "世界"},
+        trim_output = false,
+        plain = true,
+        pass_outputs = "你好 世界\n",
+    })
+    add_tests("test_echo_3", {
+        runargs = {"Play", "Musics:", "\a\a\a"},
+        trim_output = false,
+        plain = true,
+        pass_outputs = "Play Musics: \a\a\a\n",
+    })
+
 target("false")
     set_kind("binary")
     add_files("false/*.cpp")
@@ -9,3 +36,4 @@ target("true")
     set_kind("binary")
     add_files("true/*.cpp")
     add_tests("test_true")
+
